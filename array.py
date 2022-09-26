95f87d2fa6dea04c61441eedc9d61f1a450b208b
jillharrington <113885073+jillharrington@users.noreply.github.com>
2022-09-26 01:25:57 -0600

    Add files via upload

---
+++ array.py
@@ -0,0 +1,9 @@
+import numpy as np
+mixnums=np.array([3,4.2,11.1,8.9,24,17])
+print(mixnums)
+mixtypes=np.array([15,17,'fish','blue',8.88,19.7])
+print(mixtypes)
+oddarray=np.arange(1,100,2)
+print(oddarray)
+logarray=np.linspace(1,5,16)
+print(logarray)
---
+++ oddlist.py
@@ -0,0 +1,5 @@
+oddlist=[1,3,5,7,9]
+print(oddlist)
+print(len(oddlist))
+intlist=list(range(100))
+print(intlist)
